66a3741d78aceb3e9fe0de71b0eecbe869b1cc56
ToneemaDS <nemoxr27@gmail.com>
2024-08-23 12:30:22 +0400

    get_data updated

--- get_data.py
+++ get_data.py
@@ -25,20 +25,38 @@ class NetworkDAtaExtract():
         except Exception as e: 
             raise NetworkSecurityException(e,sys)
         
-    def csv_to_json_convertor(self):
+    def csv_to_json_convertor(self, file_path):
         try:
-            pass
+            data = pd.read_csv(file_path)
+            data.reset_index(drop = True, inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def pushing_data_to_mongodb(self):
+    def pushing_data_to_mongodb(self,records,database,collection):
         try:
-            pass
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records) 
+
+            return len(self.records)
+
+
         except Exception as e:
             raise NetworkSecurityException (e,sys)
         
-    if __name__ == '__main__':
-        pass
-
-        
+if __name__ == '__main__':
+    File_PATH = "D:/Network_Security/Network_Data/NetworkData.csv"
+    DATABASE = "Toneema1st"
+    COLLECTION = 'NetworkData'
+    networkobj = NetworkDAtaExtract()
+    records = networkobj.csv_to_json_convertor(File_PATH)
+    num_of_records = networkobj.pushing_data_to_mongodb(records,DATABASE,COLLECTION)
+    print(num_of_records)
 
