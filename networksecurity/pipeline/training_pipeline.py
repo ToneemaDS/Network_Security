75eeabd8ed59def44caf4bd56f0771fce36e5cc4
ToneemaDS <nemoxr27@gmail.com>
2024-09-04 12:48:50 +0400

    workflow file is updated

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -58,11 +58,14 @@ class TrainingPipeline:
             raise NetworkSecurityException(e,sys)
         
         
-    def start_data_transformation(self):
+    def start_data_transformation(self, data_validation_artifact:DataValidationArtifact):
         try:
             data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
             data_transformation = DataTransformation(data_validation_artifact = data_validation_artifact,  data_transformation_config = data_transformation_config)
-            data_transformation_artifact = data_transformation_config.initiate_data_transformation()
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
+            data_transformation_config=data_transformation_config) 
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+           
             return data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
